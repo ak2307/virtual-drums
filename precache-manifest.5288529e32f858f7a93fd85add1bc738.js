39221c2c6defee3aaada01b9b12316ce6351ead3
Abhishek Kapil <kapil_abhishek@rocketmail.com>
2020-09-22 19:02:47 -0700

    Updates

---
+++ precache-manifest.5288529e32f858f7a93fd85add1bc738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc2fde28cedc3199dd515bd98097a60",
+    "url": "/virtual-drums/index.html"
+  },
+  {
+    "revision": "0f7e82e9a0c1c0a25ebd",
+    "url": "/virtual-drums/static/css/main.dbe6bf4f.chunk.css"
+  },
+  {
+    "revision": "3782e8e9d13d7fb7fe02",
+    "url": "/virtual-drums/static/js/2.0278cbae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/virtual-drums/static/js/2.0278cbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7e82e9a0c1c0a25ebd",
+    "url": "/virtual-drums/static/js/main.44281018.chunk.js"
+  },
+  {
+    "revision": "84e9f282f32a2046c156",
+    "url": "/virtual-drums/static/js/runtime-main.25325696.js"
+  }
+]);
